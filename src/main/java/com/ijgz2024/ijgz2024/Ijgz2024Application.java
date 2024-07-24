4c4ef1d0e1ffb94a5692c4053a9f94b8d34eb8ed
IGabriel30 <ingridgabriel30@hotmail.com>
2024-07-24 10:09:23 -0600

    first commit

---
+++ src/main/java/com/ijgz2024/ijgz2024/Ijgz2024Application.java
@@ -0,0 +1,13 @@
+package com.ijgz2024.ijgz2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ijgz2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ijgz2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijgz2024/ijgz2024/Ijgz2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijgz2024.ijgz2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ijgz2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
